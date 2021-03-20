6371366f6c6e51a26f836e7631cf7f9bba5987f6
sanuzi <80156683+sanuzi@users.noreply.github.com>
2021-03-20 14:18:32 -0400

    Add files via upload

---
+++ backup.1password.online.account.to.local.vault.ps1
@@ -0,0 +1,62 @@
+#                              
+# 1password online vault backup tool (** can set to automatically run by adding as a task via windows task scheduler)
+#
+#------------------------------------
+#
+# PRE-REQS                     
+#
+# * download op cli (url here) and add to PATH
+#
+# * use op cli to manually log into 1password via cli for the first time (you won't have to do this again)
+#   
+#    . run the following without quotes: 'op signin my.1password.com email@domain.com'
+#    . this will add your account to the windows cache; from this point on, you can execute this shorthand to sign in: 'op signin'
+
+ param (
+    [string]$email    = "",
+    [string]$password = "",
+    [string]$aeskey   = ""
+ )
+
+if ($email -eq "" -and $password -eq "") {
+    echo ""
+	echo "   !!! ERROR: E-mail and/or password missing; exiting script without running !!!"
+}
+else {
+    # login to 1password account and set auth token as temporary PATH var
+    Invoke-Expression $($password | op signin)
+
+    #scrape vault items for all items uuids via regex
+    $allItems = op list items;
+    $uuids = [regex]::Matches($allItems, '"uuid":([^"]*"[^"]*)')
+
+    #get item details for each item
+    $itemDetails = [System.Collections.ArrayList]@()
+    foreach ($match in $uuids)
+    {
+        
+        $itemDetail = op get item $match.Groups[1].Value;
+        $itemDetails.Add($itemDetail);
+    }
+
+    $rawOutput = $itemDetails  | & {"$input"}
+
+    # encrypt with aes sha 256 if key is provided
+    if ($aeskey -ne $null) {
+
+        $scriptRoot = $PSScriptRoot;
+        if ($psISE)
+        {
+            $scriptRoot = Split-Path -Path $psISE.CurrentFile.FullPath        
+        }
+
+		$aesModulePath = $scriptRoot + "\dependencies\PowershellAes.ps1"
+        import-module $aesModulePath -Verbose:$false
+        $finalizedOutput = Encrypt-String $aeskey $rawOutput
+	}
+    else {
+        $finalizedOutput = $itemDetails
+    }
+
+    $finalizedOutput > $PSScriptRoot\encrypted.vault.backup.txt
+}
